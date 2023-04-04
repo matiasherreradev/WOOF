1b6cb653194f5d71bed2137f57aa1c3bb67ce7ac
senderos3d <senderos3d@gmail.com>
2023-04-04 18:07:32 -0300

    2da parte con imagenes

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Vamos con ese proyecto, cuando cueste a no decaer y levantar la cabeza!
 
-a los 5 m instala create router???
+---la imagen del perro hay que buscarle la manera para que quede guardada en la compu
+---poner fondo de pantalla con huellas de perros y huesos
 
-vamos 14 minutos
+
+vamos 25 minutos
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-
+import { Link } from "react-router-dom";
 
 export default function Home() {
   const [dogs, setDogs] = useState([]);
@@ -27,11 +27,43 @@ export default function Home() {
         </h1>
       ) : (
         <>
-          <section className="p-8 max-w-6xl mx-auto">
-           <img className="h-24 w-24"   src="http://127.0.0.1:5500/src/svg/dog-training.svg" alt="My SVG" /> 
-                    <h1 className="flex items-center justify-center text-amber-700 text-center text-3xl px-5 h-screen font-bold ">
-          firulApp
-        </h1>
+          <section className="p-8 max-w-8xl mx-auto">
+            <div className="text-center">
+              <h1 className="flex items-center justify-center text-amber-700 text-center text-3xl px-5  font-bold lg:text-5xl">
+                firulApp
+              </h1>
+              <p className="my-6">
+                This app is powered by{" "}
+                <a
+                  className="text-indigo-700 underline active:text-orange-500"
+                  href="https://thedogapi.com"
+                >
+                  The dog API
+                </a>
+              </p>
+              <form className="mx-auto max-w-xl my-12">
+                <input
+                  className="w-full rounded-md shadow-md px-4 py-2 text-  bg-orange-200 text-amber-950"
+                  type="text"
+                  name="search"
+                  id="search"
+                  placeholder="Search for a dog / breed"
+                />
+              </form>
+            </div>
+
+            <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 mx-20">
+              {dogs.map((dog) => (
+                <Link to={`/${dog.name}`}  key={dog.id}>
+                   <article  className="bg-slate-300 rounded-lg">
+                  <img src={dog.image.url} alt={dog.name} loading="lazy"className="rounded-lg md:h-72 md:w-full object-cover"/>
+                  <h3 className="my-2 text-center font-bold text-lg">{dog.name} </h3>
+                  <p className="bg-slate-600 text-center text-white py-2">Bred For: {dog.bred_for}</p>
+                </article>
+                </Link>
+             
+              ))}
+            </div>
           </section>
         </>
       )}
